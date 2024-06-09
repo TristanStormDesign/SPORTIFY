52f4d0005b5fb06547d8534ed23b8c9a0c9ef285
TristanStormDesign <tristanstormdesign@gmail.com>
2024-06-09 02:52:27 +0200

    admin update

--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -1,6 +1,8 @@
+// Admin.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './Admin.css';
+import LoginModal from './LoginModal';
 
 function Admin() {
     const [products, setProducts] = useState([]);
@@ -9,12 +11,14 @@ function Admin() {
     const [isEditing, setIsEditing] = useState(false);
     const [searchQuery, setSearchQuery] = useState('');
     const [currentPage, setCurrentPage] = useState(1);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [showLoginModal, setShowLoginModal] = useState(true);
     const productsPerPage = 4;
-  
+
     useEffect(() => {
       fetchProducts();
     }, []);
-  
+
     const fetchProducts = async () => {
       try {
         const response = await axios.get('http://localhost:5000/api/products');
@@ -24,13 +28,13 @@ function Admin() {
         console.error('Error fetching products:', error);
       }
     };
-  
+
     const handleSearch = (query) => {
       setSearchQuery(query);
       const filtered = products.filter(product => product.name.toLowerCase().includes(query.toLowerCase()));
       setFilteredProducts(filtered);
     };
-  
+
     const handleAddProduct = async (product) => {
       try {
         await axios.post('http://localhost:5000/api/products', product);
@@ -39,7 +43,7 @@ function Admin() {
         console.error('Error adding product:', error);
       }
     };
-  
+
     const handleEditProduct = async (product) => {
       try {
         await axios.put(`http://localhost:5000/api/products/${product._id}`, product);
@@ -50,7 +54,7 @@ function Admin() {
         console.error('Error editing product:', error);
       }
     };
-  
+
     const handleDeleteProduct = async (id) => {
       try {
         await axios.delete(`http://localhost:5000/api/products/${id}`);
@@ -59,42 +63,42 @@ function Admin() {
         console.error('Error deleting product:', error);
       }
     };
-  
+
     const handleEditClick = (product) => {
       setSelectedProduct(product);
       setIsEditing(true);
     };
-  
+
     const handleFormClose = () => {
       setIsEditing(false);
       setSelectedProduct(null);
     };
-  
+
     const paginate = (pageNumber) => setCurrentPage(pageNumber);
-  
+
     const ProductForm = ({ product, onClose, onSave }) => {
       const [formData, setFormData] = useState({
         name: '',
         price: '',
         description: '',
       });
-  
+
       useEffect(() => {
         if (product) {
           setFormData(product);
         }
       }, [product]);
-  
+
       const handleChange = (e) => {
         const { name, value } = e.target;
         setFormData({ ...formData, [name]: value });
       };
-  
+
       const handleSubmit = (e) => {
         e.preventDefault();
         onSave(formData);
       };
-  
+
       return (
         <div className="product-form">
           <h2>{product ? 'Edit Product' : 'Add Product'}</h2>
@@ -131,50 +135,62 @@ function Admin() {
         </div>
       );
     };
-  
+
     // Pagination logic
     const indexOfLastProduct = currentPage * productsPerPage;
     const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
     const currentProducts = filteredProducts.slice(indexOfFirstProduct, indexOfLastProduct);
-  
+
     return (
       <div>
-        <header className="products-header">
-          <input
-            type="text"
-            placeholder="Search products..."
-            value={searchQuery}
-            onChange={(e) => handleSearch(e.target.value)}
-          />
-        </header>
-        {isEditing && (
-          <ProductForm
-            product={selectedProduct}
-            onClose={handleFormClose}
-            onSave={selectedProduct ? handleEditProduct : handleAddProduct}
+        {showLoginModal && (
+          <LoginModal
+            onClose={() => setShowLoginModal(false)}
+            onLogin={() => setIsAuthenticated(true)}
           />
         )}
-        <div className="products-container">
-          {currentProducts.map(product => (
-            <div key={product._id} className="product-block">
-              <span>{product.name}</span>
-              <button onClick={() => handleEditClick(product)}>Edit</button>
-              <button onClick={() => handleDeleteProduct(product._id)}>Delete</button>
+        {isAuthenticated ? (
+          <>
+            <header className="products-header">
+              <input
+                type="text"
+                placeholder="Search products..."
+                value={searchQuery}
+                onChange={(e) => handleSearch(e.target.value)}
+              />
+            </header>
+            {isEditing && (
+              <ProductForm
+                product={selectedProduct}
+                onClose={handleFormClose}
+                onSave={selectedProduct ? handleEditProduct : handleAddProduct}
+              />
+            )}
+            <div className="products-container">
+              {currentProducts.map(product => (
+                <div key={product._id} className="product-block">
+                  <span>{product.name}</span>
+                  <button onClick={() => handleEditClick(product)}>Edit</button>
+                  <button onClick={() => handleDeleteProduct(product._id)}>Delete</button>
+                </div>
+              ))}
+              {!isEditing && (
+                <div className="product-block add-product-block" onClick={() => setIsEditing(true)}>
+                  <span>Add New Product</span>
+                </div>
+              )}
             </div>
-          ))}
-          {!isEditing && (
-            <div className="product-block add-product-block" onClick={() => setIsEditing(true)}>
-              <span>Add New Product</span>
+            <div className="pagination">
+              {Array.from({ length: Math.ceil(filteredProducts.length / productsPerPage) }, (_, i) => (
+                <button key={i + 1} onClick={() => paginate(i + 1)}>{i + 1}</button>
+              ))}
             </div>
-          )}
-        </div>
-        <div className="pagination">
-          {Array.from({ length: Math.ceil(filteredProducts.length / productsPerPage) }, (_, i) => (
-            <button key={i + 1} onClick={() => paginate(i + 1)}>{i + 1}</button>
-          ))}
-        </div>
+          </>
+        ) : (
+          <div>Please log in to view the admin content.</div>
+        )}
       </div>
     );
   };
 
-export default Admin
+export default Admin;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,41 +1,52 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import './Home.css';
-import videoSource from '../images/HeaderVideo.mp4';
-import middleImage from '../images/middleImage.png';
-import sportLogo1 from '../images/sportLogo1.png';
-import sportLogo2 from '../images/sportLogo2.png';
-import sportLogo3 from '../images/sportLogo3.png';
-import sportLogo4 from '../images/sportLogo4.png';
-import sportLogo5 from '../images/sportLogo5.png';
-import sportLogo6 from '../images/sportLogo6.png';
-import storeImage from '../images/storeImage.png';
+import headerImage from '../images/HeaderImage.png';
+import nikeText from '../images/NikeText.png';
+import underArmorText from '../images/UnderArmorText.png';
+import adidasText from '../images/AdidasText.png';
+import pumaText from '../images/PumaText.png';
 
 const Home = () => {
-    return (
-        <div>
-            <header className="header">
-                <video autoPlay loop muted className="header-video">
-                    <source src={videoSource} type="video/mp4" />
-                </video>
-                <img src={middleImage} alt="Middle" className="middle-image" />
-            </header>
-            <section className="logos-section">
-                <div className="logo-block"><img src={sportLogo1} alt="Sport Logo 1" /></div>
-                <div className="logo-block"><img src={sportLogo2} alt="Sport Logo 2" /></div>
-                <div className="logo-block"><img src={sportLogo3} alt="Sport Logo 3" /></div>
-                <div className="logo-block"><img src={sportLogo4} alt="Sport Logo 4" /></div>
-                <div className="logo-block"><img src={sportLogo5} alt="Sport Logo 5" /></div>
-                <div className="logo-block"><img src={sportLogo6} alt="Sport Logo 6" /></div>
-            </section>
-            <section className="about-section">
-                <img src={storeImage} alt="Store" className="store-image" />
-                <div className="about-text">
-                    <h2>About Sportify</h2>
-                    <p>Welcome to Sportify, your number one source for all things sports. We're dedicated to giving you the very best of sporting goods, with a focus on quality, customer service, and uniqueness. Founded in 2021, Sportify has come a long way from its beginnings in a home office. We hope you enjoy our products as much as we enjoy offering them to you.</p>
+  const slides = [
+    { textImage: nikeText },
+    { textImage: underArmorText },
+    { textImage: adidasText },
+    { textImage: pumaText },
+  ];
+
+  return (
+    <div>
+      <header className="header">
+        <img src={headerImage} alt="Header" className="header-image" />
+        <Carousel
+          className="product-carousel"
+          showThumbs={false}
+          showStatus={false}
+          showArrows={false}
+          autoPlay
+          interval={5000}
+          infiniteLoop
+        >
+          {slides.map((slide, index) => (
+            <div key={index} className="carousel-item">
+              <div className="carousel-content">
+                <div className="text-section">
+                  <img src={slide.textImage} alt="Text" className="text-image" />
+                  <Link to="/products" className="view-product-button">
+                    View Product
+                  </Link>
                 </div>
-            </section>
-        </div>
-    );
+              </div>
+            </div>
+          ))}
+        </Carousel>
+      </header>
+      <section className="blank-section"></section>
+    </div>
+  );
 };
 
 export default Home;
